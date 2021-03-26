0c7b9419b7a0caa34eba5cb6b2acc1812c45d4cf
Daniel Primo <delineas@gmail.com>
2021-03-26 20:57:07 +0100

    WIP

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use App\Domain\UseCases\ReplyToNapalm;
 use Telegram\Bot\Objects\Update;
+use Illuminate\Support\Facades\Log;
+use App\Domain\UseCases\CaptureLink;
+use App\Domain\UseCases\ReplyToNapalm;
+use App\Domain\UseCases\WelcomeNewUser;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
@@ -15,9 +17,11 @@ class TelegramController extends Controller
         /** @var Update $update */
         $update = Telegram::getWebhookUpdates();
 
-        Log::info($update->getChat()['id']);
+        Log::info($update->getMessage()->get('from'));
 
         (new ReplyToNapalm($update))();
+        (new WelcomeNewUser($update))();
+        (new CaptureLink($update))();
         
         response()->json(['ok' => true])->send();
     }
--- config/botmalandriner.php
+++ config/botmalandriner.php
@@ -2,5 +2,6 @@
 
 return [
   'chat_id' => env('TELEGRAM_CHAT_GROUP_ID'),
-  'reply_napalm' => '*¡Hoy hay directo\!* [Napalm](http://cdn.lowgif.com/full/193253edb4586a09-.gif)'
+  'reply_napalm' => '*¡Hoy hay directo\!* [Napalm](http://cdn.lowgif.com/full/193253edb4586a09-.gif)',
+  'welcome_new_user' => '¡Bienvenid@ malandriner @first_name @last_name\! La tortilla de patata, ¿con o sin cebolla\? Cuidado con tu respuesta\.\.\.'
 ];
